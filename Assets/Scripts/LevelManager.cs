0281b41f56249df3b333513a6c715a8fa96222fd
Isaiah Wijaya <wijiisaiah@gmail.com>
2017-03-05 00:37:34 -0800

    First Commit of this Project - Already added VR Camera to Scenes - Contains skeleton of Main Menu, level and End game sc:q :`

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    private static int MenuIndex = 0;
+    private static int PlayIndex = 1;
+    private static int QuitIndex = 2;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(PlayIndex);
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(QuitIndex);
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene(MenuIndex);
+    }
+}
