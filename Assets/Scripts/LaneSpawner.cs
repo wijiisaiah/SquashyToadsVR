7991184062efcf3203fe3eccd384d370db375c91
Isaiah Wijaya <wijiisaiah@gmail.com>
2017-03-05 23:51:01 -0800

    Randomly generated lanes, spawn random density of trees on woodland lanes

---
+++ Assets/Scripts/LaneSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaneSpawner : MonoBehaviour {
+
+    public GameObject[] lanePrefabs;
+    public int maxOffset = 500;
+
+    // Use this for initialization
+    void Start () {
+        int offset = 0;
+        while (offset < maxOffset)
+        {
+            CreateRandomLane(offset);
+            offset += 10;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void CreateRandomLane(float offset)
+    {
+        int laneIndex = Random.Range(0, lanePrefabs.Length);
+        var lane = Instantiate(lanePrefabs[laneIndex]);
+
+        Debug.Log(lane.name);
+        lane.transform.parent = transform;
+        lane.transform.Translate(0, 0, offset);
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/TreeSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeSpawner : MonoBehaviour {
+
+
+    public GameObject treePrefab;
+    public int minTrees = 1;
+    public int maxTrees = 6;
+    // Use this for initialization
+    void Start () {
+        CreateTrees();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void CreateTrees()
+    {
+        var numberOfTrees = Random.Range(minTrees, maxTrees);
+        for (int i = 0; i <= numberOfTrees; i++)
+        {
+            var tree = Instantiate(treePrefab);
+            tree.transform.parent = transform;
+            tree.transform.localPosition= new Vector3(Random.Range(-50, 50), 0, Random.Range(-5, 5));
+        }
+        
+    }
+
+
+}
